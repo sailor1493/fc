2f7c0135178e658419ecc9a1bf14b515af91d74c
sailor1493 <99chanwoo@gmail.com>
2021-12-23 11:06:10 +0900

    ADD type intro files

---
+++ week1/type.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	char c = '0';
+	int num = 48;
+	printf("c in text\t: %c\n", c);
+	printf("c in memory\t: %u\n", c);
+	printf("num in memory\t: %d\n", num);
+	return 0;
+}
